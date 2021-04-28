9e27cce00c8c138ebf4272d3e2c061ca32994121
Trevor McGlaflin <trevor.mcglaflin@uvm.edu>
2021-04-28 11:49:44 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
+Trevor McGlaflin
+
 Welcome to Rectangle Goalie!
 
-The goal of the game is to block the shots by using the keyboard up and down keys.
+Summary: This program utilizes the rect and circle classes used in the guided projects to create
+an interactive goalie game with multiple levels. There are 4 different screens throughout the game:
+1. Welcome Screen: welcomes user prompts them to enter 'p' to start
+2. Game play Screen: user controls rectangle with up and down arrow keys to block shots
+3. In Between Level Screen: tells user what level they are on, prompts them to hit spacebar to move on
+4. You lost Screen: if they let a ball in, they lose. They can start over by hitting 'p'
 
+NOTE: you can hit 'p' at any point during game to start over
+
+
+User Guide: The goal of the game is to block the shots by using the keyboard up and down keys.
 Each round your rectangle will get smaller, the balls will come faster, and there will be more balls!
+If you let a ball past you, you lose.
 
-Good Luck
+Helpful hints: 
+- while waiting for next ball, reposition yourself to center of screen.
+- the first ball each round comes at you quick, be ready!
+
+
+
--- graphics.cpp
+++ graphics.cpp
@@ -11,18 +11,12 @@ using namespace std;
 
 
 GLdouble width, height;
-int wd, ballSize;
+int wd, ballSize, userHeight, userWidth, points, level, addedPointsPerLevel, ballSpeed;
 vector<Circle> balls;
 Rect user;
 point2D userCenter;
 dimensions userDimensions;
-int userHeight;
-int userWidth;
 bool ballMovesRight;
-int points;
-int level;
-int addedPointsPerLevel;
-int ballSpeed;
 color ballColor;
 screenLetter screen;
 const color skyBlue(77/255.0, 213/255.0, 240/255.0);
@@ -52,7 +46,6 @@ bool circleHitRect(Circle &c, Rect &r) {
     }
     return false;
 }
-
 
 void init() {
     srand(time(0));
@@ -86,29 +79,19 @@ void init() {
     screen = w;
 }
 
-/* Initialize OpenGL Graphics */
 void initGL() {
-    // Set "clearing" or background color
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Black and opaque
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 }
 
-/* Handler for window-repaint event. Call back when the window first appears and
- whenever the window needs to be re-painted. */
 void display() {
-    // Tell OpenGL to use the whole window for drawing
-    glViewport(0, 0, width, height); // DO NOT CHANGE THIS LINE
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0.0, width, height, 0.0, -1.f, 1.f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
-    // Do an orthographic parallel projection with the coordinate
-    // system set to first quadrant, limited by screen/window size
-    glMatrixMode(GL_PROJECTION); // DO NOT CHANGE THIS LINE
-    glLoadIdentity(); // DO NOT CHANGE THIS LINE
-    glOrtho(0.0, width, height, 0.0, -1.f, 1.f); // DO NOT CHANGE THIS LINE
-
-    // Clear the color buffer with current clearing color
-    glClear(GL_COLOR_BUFFER_BIT); // DO NOT CHANGE THIS LINE
-
-    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); // DO NOT CHANGE THIS LINE
-
+    // display screen based on screenLetter enum
     if (screen == w) {
         glColor3f(1, 1, 1);
         glRasterPos2i(200, 200);
@@ -140,7 +123,7 @@ void display() {
             glutBitmapCharacter(GLUT_BITMAP_8_BY_13, letter);
         }
     }
-    glFlush();  // Render now
+    glFlush();
 }
 
 
@@ -191,8 +174,6 @@ void kbd(unsigned char key, int x, int y) {
         }
         user.setCenter(userCenter);
         user.setColor(colors[rand() % colors.size()]);
-
-
         screen = p;
     }
 
@@ -232,18 +213,14 @@ void cursor(int x, int y) {
     glutPostRedisplay();
 }
 
-// button will be GLUT_LEFT_BUTTON or GLUT_RIGHT_BUTTON
-// state will be GLUT_UP or GLUT_DOWN
 void mouse(int button, int state, int x, int y) {
-    // D3: What does this code do? What will it look like? Where will it be?
+
      string message = "You clicked the mouse at coordinate (" + to_string(x) + ", " + to_string(y) + ")";
      glColor3f(1, 1, 1);
      glRasterPos2i(0, height);
      for (char letter : message) {
          glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, letter);
      }
-
-
      glutPostRedisplay();
 }
 
@@ -291,43 +268,21 @@ void timer(int dummy) {
     glutTimerFunc(50, timer, dummy);
 }
 
-/* Main function: GLUT runs as a console application starting at main()  */
 int main(int argc, char** argv) {
-
     init();
-
     glutInit(&argc, argv);
-
     glutInitDisplayMode(GLUT_RGBA);
-
     glutInitWindowSize((int)width, (int)height);
-    glutInitWindowPosition(0, 0); // Position the window's initial top-left corner
-    /* create the window and store the handle to it */
+    glutInitWindowPosition(0, 0);
     wd = glutCreateWindow("Rectangle Goalie!" /* title */ );
-
-    // Register callback handler for window re-paint event
     glutDisplayFunc(display);
-
-    // Our own OpenGL initialization
     initGL();
-
-    // register keyboard press event processing function
-    // works for numbers, letters, spacebar, etc.
     glutKeyboardFunc(kbd);
     glutKeyboardUpFunc(kbdUp);
-
-    // register special event: function keys, arrows, etc.
     glutSpecialFunc(kbdS);
-
-    // handles mouse movement
     glutPassiveMotionFunc(cursor);
-
-    // handles mouse click
     glutMouseFunc(mouse);
-
-    // handles timer
     glutTimerFunc(0, timer, 0);
-    // Enter the event-processing loop
     glutMainLoop();
     return 0;
 }
